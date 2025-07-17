d45d598f0879e0dbe2aca7e1bef1b4a21aad705d
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-07-17 12:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
-  //your JS code here.
+  if (!str) return false;
+
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{2,}$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
